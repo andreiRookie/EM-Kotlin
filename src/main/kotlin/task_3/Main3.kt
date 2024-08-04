219b83eab7c53bc19cb0c2a3b726dd4ee85ecaca
AndreiRookie <andrey.idem@gmail.com>
2024-08-04 16:14:37 +0400

    task_3 done

---
+++ src/main/kotlin/task_3/Main3.kt
@@ -0,0 +1,69 @@
+package task_3
+
+fun main() {
+
+    val sortedList = shakerSort(listToSort)
+    println("SortedList:\n$sortedList")
+
+}
+
+private fun shakerSort(list: List<Int?>?): List<Int?> {
+    require(list != null) { "List must not be null" }
+
+    val resultList = list.toMutableList()
+
+    // list borders
+    var listStartMark = 1
+    var listEndMark = resultList.size - 1
+
+    while (listStartMark <= listEndMark) {
+
+        // from end to start
+        for (i in listEndMark downTo listStartMark) {
+            when {
+                resultList[i - 1] == null -> {
+                    resultList.swap(iOld = i - 1, iNew = i)
+                    continue
+                }
+
+                resultList[i] != null && resultList[i - 1]!! > resultList[i]!! -> {
+                    resultList.swap(iOld = i - 1, iNew = i)
+                    continue
+                }
+            }
+        }
+        listStartMark++
+
+        // from start to end
+        for (i in listStartMark until listEndMark) {
+
+            when {
+                resultList[i] == null -> {
+                    resultList.swap(iOld = i, iNew = i + 1)
+                    continue
+                }
+
+                resultList[i + 1] != null && resultList[i]!! > resultList[i + 1]!! -> {
+                    resultList.swap(iOld = i, iNew = i + 1)
+                    continue
+                }
+            }
+        }
+        listEndMark--
+
+        println("Iteration ${listStartMark - 1}")
+    }
+
+    return resultList
+}
+
+private fun MutableList<Int?>.swap(iOld: Int, iNew: Int) {
+    val buffer = this[iOld]
+    this[iOld] = this[iNew]
+    this[iNew] = buffer
+}
+
+private val listToSort: List<Int?> = listOf(
+    1, null, null, 1234, null, null, 22222, 3434, 50000, null, 66,
+    100, null, null, 555, null, 777, 12, null, null
+)
